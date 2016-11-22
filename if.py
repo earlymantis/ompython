626d648a2b2f62fefc5c6488bf70a1b9683061b3
earlymantis <alallos@me.com>
2016-11-21 19:40:30 -0500

    Add files via upload
    if, elif, else practice

---
+++ if.py
@@ -0,0 +1,13 @@
+answer = input("Do you want to hear a joke? ")
+
+yes = ["Yes", "yes", "Y", "y"]
+no = ["No", "no", "N", "n"]
+
+if answer in yes:
+	print("I'm against picketing, but I don't know how to show it.")
+	#Mitch Hedburg (RIP)
+elif answer in no:
+	print("Fine.")
+
+else:
+	print("I'm sorry, I don't understand.")
